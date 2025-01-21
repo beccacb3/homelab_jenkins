d22c06f3338c9a71d2a10052b6497e6777247bd9
Becca <beccacb3@gmail.com>
2025-01-20 20:28:53 -0600

    create parameters for image and tag

--- src/Groovy/dockerBuild.groovy
+++ src/Groovy/dockerBuild.groovy
@@ -7,16 +7,30 @@ pipeline{
             defaultContainer 'shell'
         }
     }
-    environment {
-        IMAGE_NAME = 'realestate-app'
-        BRANCH = 'development'
-        TAG = "${env.BRANCH}-${env.BUILD_NUMBER}"
-        // KUBECONFIG = credentials('kubeconfig-credential-id') // Store kubeconfig in Jenkins credentials
+    // environment {
+    //     IMAGE_NAME = 'realestate-app'
+    //     BRANCH = 'development'
+    //     TAG = "${env.BRANCH}-${env.BUILD_NUMBER}"
+    // }
+    parameters {
+        string: 'image_name',
+        defaultValue: params.image_name ? : '',
+        description: ''
+    }
+    parameters {
+        string: 'branch',
+        defaultValue: params.branch ? : '',
+        description: ''
+    }
+    parameters {
+        string: 'tag',
+        defaultValue: params.tag ? : '',
+        description: ''
     }
     stages{
         stage('Checkout') {
             steps {
-                git branch: env.BRANCH, 
+                git branch: params.branch, 
                     url: 'https://github.com/cherpin00/compass-scraping',
                     credentialsId: '4da91a3b-816d-48c0-8aa0-ce7e11e13243'
             }
@@ -24,40 +38,18 @@ pipeline{
         stage('Docker Build and Push') {
             steps {
                 script {
-                    // Log in to Docker registry using credentials stored in Jenkins
+                    def tag = "${params.branch}-${env.BUILD_NUMBER}"
                     withCredentials([usernamePassword(credentialsId: 'a453e044-6a68-4edb-a82e-b26ffe9054af', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
                         sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
                     }
-                    
-                    // Build the Docker image
-                    sh 'cd frontend; docker build . -t docker.io/cherpin/$IMAGE_NAME:$TAG'
-                    
-                    // Push the image to the Docker registry
-                    //image.push()
+                    sh "cd frontend; docker build . -t docker.io/cherpin/${params.image_name}:$tag"
                 }
             }
         }
         stage('Docker Push') {
             steps {
-                sh 'docker push docker.io/cherpin/$IMAGE_NAME:$TAG'
+                sh "docker push docker.io/cherpin/${image_names}:${tag}"
             }
-        }
-        stage('Deploy to Kubernetes') {
-            steps {
-                sh '''
-                kubectl set image deployment/react-app \
-                    your-container-name=$IMAGE_NAME:$TAG -n realestate-app-$BRANCH
-                kubectl rollout status deployment/react-app -n realestate-app-$BRANCH
-                '''
-            }
-        }
-    }
-    post {
-        success {
-            echo 'Deployment successful!'
-        }
-        failure {
-            echo 'Deployment failed!'
         }
     }
 }
