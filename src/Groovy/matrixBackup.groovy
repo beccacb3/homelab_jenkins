87015df537020fa5ac683181ceff1e2a44b19b34
Caleb Herpin <caleb.herpin@gmail.com>
2025-02-11 22:05:56 -0600

    Testing backup pipeline

--- src/Groovy/matrixBackup.groovy
+++ src/Groovy/matrixBackup.groovy
@@ -19,9 +19,9 @@ pipeline {
             steps {
                 script {
                 	sh """
-                		volumeMount=$(kubectl --namespace matrix get deployment matrix -o json | jq -r '.spec.template.spec.volumes[] | select(has("persistentVolumeClaim")).name') 
-                		podName=$(kubectl --namespace matrix get pods | grep matrix | awk '{print $1}')
-                		dataPath=$(kubectl --namespace matrix get deployment matrix -o json | jq -r ".spec.template.spec.containers[].volumeMounts[] | select(.name == \"${volumeMount}\").mountPath")
+                		volumeMount=\$(kubectl --namespace matrix get deployment matrix -o json | jq -r '.spec.template.spec.volumes[] | select(has("persistentVolumeClaim")).name') 
+                		podName=\$(kubectl --namespace matrix get pods | grep matrix | awk '{print \$1}')
+                		dataPath=\$(kubectl --namespace matrix get deployment matrix -o json | jq -r ".spec.template.spec.containers[].volumeMounts[] | select(.name == \"${volumeMount}\").mountPath")
                 		kubectl --namespace matrix cp ${podName}:${dataPath} data.bak
                 	"""
                 }
